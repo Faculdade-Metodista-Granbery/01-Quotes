4dcdaf77a6133b182fcd6ad219293c0a0f77dd01
lesimoes <lesimoes0@gmail.com>
2021-03-14 09:41:57 -0300

    adicionado loader

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function onComplete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     if (!quote.author) {
         authorText.textContent = 'Unknown';
@@ -21,10 +33,11 @@ function newQuote() {
         quoteText.classList.remove('long-quote')
     }
     quoteText.textContent = quote.text;
-
+    onComplete();
 }
 
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
